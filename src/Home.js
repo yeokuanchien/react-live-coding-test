c6d18e6b0832b6e78c74820d3e4fa6d7d9e96fbf
Hau Cherng <hau.cherng.wong@accenture.com>
2021-06-01 23:53:07 +0800

    Initial Commit

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,6 @@ function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavLink to="/pokedex">
           <img
             hidden={!isReady}
             src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
@@ -17,7 +16,6 @@ function Home() {
             alt="logo"
             style={{ padding: "10px" }}
           />
-        </NavLink>
         <b>
           Requirement: Try to show the hidden button and make it clickable that
           goes to /pokedex when the input below is "Ready!" remember to hide the
