58caac6cde48260d76b4e7a0af0eb0a791dd8607
Hau Cherng <hau.cherng.wong@accenture.com>
2021-06-02 16:39:27 +0800

    README update.

--- src/Home.js
+++ src/Home.js
@@ -9,13 +9,13 @@ function Home() {
   return (
     <div className="App">
       <header className="App-header">
-          <img
-            hidden={!isReady}
-            src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
-            className="App-logo"
-            alt="logo"
-            style={{ padding: "10px" }}
-          />
+        <img
+          hidden={!isReady}
+          src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
+          className="App-logo"
+          alt="logo"
+          style={{ padding: "10px" }}
+        />
         <b>
           Requirement: Try to show the hidden button and make it clickable that
           goes to /pokedex when the input below is "Ready!" remember to hide the
